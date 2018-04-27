8028b722c82802177cbfa62953122584bd115ee8
avialee <liyuxin@liyuxins-MacBook-Pro.local>
2018-04-27 16:05:15 -0700

    user register added (password confirmation missing)

--- nextseq_app/urls.py
+++ nextseq_app/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from nextseq_app import views
+from django.urls import path
+from . import views
 
+app_name = 'nextseq_app'
 urlpatterns = [
-    url(r'^$', views.index,name='index') 
+    url(r'^$', views.index,name='index'),
+    path('register/', views.UserFormView.as_view(), name='register'),
 ]
--- nextseq_app/views.py
+++ nextseq_app/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserForm
+from django.views.generic import View
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
-    
+
+class UserFormView(View):
+	form_class = UserForm
+	template_name = 'nextseq_app/registration.html'
+
+	def get(self, request):
+		form = self.form_class(None)
+		return render(request, self.template_name,{'form':form})
+
+	def post(self, request):
+		form = self.form_class(request.POST)
+
+		if form.is_valid():
+			user = form.save(commit=False)
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			user.set_password(password)
+			user.save()
+
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				if user.is_active:
+					login(request, user)
+					return redirect('nextseq_app:index')
+		return render(request, self.template_name,{'form':form})
