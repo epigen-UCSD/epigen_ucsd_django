6b697370a63f493887d60d0138273e570786c7ba
avialee <liyuxin@liyuxins-MacBook-Pro.local>
2018-06-13 09:32:42 -0700

    example script to store data into database

---
+++ scripts/barcodeimport.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "epigen_ucsd_django.settings")
+import django
+django.setup()
+from nextseq_app.models import Barcode
+
+def main():
+	
+	index = {}
+	i= 0
+	with open('barcodes.csv','r') as f:
+		for line in f:
+			fields = line.strip('\n').split('\t')
+			if fields[0] in index.keys():
+				if fields[0]!= index[fields[0]]:
+					print(fields[0]+' sequence conflict!')
+					sys.exit()
+			else:
+				index[fields[0]] = fields[1]
+				i += 1
+		print(i)
+	#tobecreatedlist = []
+	for item in index.keys():
+		#tobecreatedlist.append(Barcode(indexid=item, indexseq=index[item]))
+		Barcode.objects.get_or_create(indexid=item, indexseq=index[item])
+	#Barcode.objects.bulk_create(tobecreatedlist)
+	#Barcode.objects.all().delete()
+
+
+
+if __name__ == '__main__':
+	main()
