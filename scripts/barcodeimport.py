82e83e3fe326771aae42a8ea06567afeb30ecfbe
biomystery <zhangc518@gmail.com>
2018-07-03 09:02:26 -0700

    updated import barcodes script && still need fix setting

--- scripts/barcodeimport.py
+++ scripts/barcodeimport.py
@@ -1,38 +1,30 @@
 #!/usr/bin/env python
+# Time-stamp: <2018-07-03 09:01:48>
+
 import os
 import sys
-#print(os.path.abspath(__file__))
-basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(basedir)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "epigen_ucsd_django.settings")
-
 import django
+os.chdir("../")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "epigen_ucsd_django.settings")
 django.setup()
 from nextseq_app.models import Barcode
 
+def getArgs():
+        import argparse
+        parser = argparse.ArgumentParser(description='Import barcodes script.')
+        parser.add_argument('-b','--barcode_file', dest='barcode_file', 
+                    help='input barcode file  (csv format)')
+
+        args = parser.parse_args()
+        return args.barcode_file
+
 def main():
-	
-	index = {}
-	i= 0
-	with open('barcodes.csv','r') as f:
-		for line in f:
-			fields = line.strip('\n').split('\t')
-			if fields[0] in index.keys():
-				if fields[0]!= index[fields[0]]:
-					print(fields[0]+' sequence conflict!')
-					sys.exit()
-			else:
-				index[fields[0]] = fields[1]
-				i += 1
-		print(i)
-	#tobecreatedlist = []
-	for item in index.keys():
-		#tobecreatedlist.append(Barcode(indexid=item, indexseq=index[item]))
-		Barcode.objects.get_or_create(indexid=item, indexseq=index[item])
-	#Barcode.objects.bulk_create(tobecreatedlist)
-	#Barcode.objects.all().delete()
-
-
+        with io.open('./data/nextseq_app/barcodes/JYH_2018-07-02_barcodes.csv','r',encoding='utf-8') as f: lines = f.read().splitlines()
+        indexes = {l.replace(u'\ufeff','').split(',')[0]:l.replace(u'\ufeff','').split(',')[1]for l in lines}
+        for k,v in indexes.items(): obj,created=Barcode.objects.get_or_create(indexid=k, indexseq=v)
 
 if __name__ == '__main__':
 	main()
+
+
+
