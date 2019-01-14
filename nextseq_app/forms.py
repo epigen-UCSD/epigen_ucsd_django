193d14309a4412a1fe58a24951c24fdf08c6ef7b
biomystery <zhangc518@gmail.com>
2019-01-14 13:48:07 -0800

    changed default to PE@nextseq@addbulk

--- nextseq_app/forms.py
+++ nextseq_app/forms.py
@@ -2,45 +2,49 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 #from django.utils.translation import gettext_lazy as _
-from .models import RunInfo,LibrariesInRun
+from .models import RunInfo, LibrariesInRun
 from django.forms import ModelForm
 
 # class DateInput(forms.DateInput):
 # 	input_type = 'date'
 
-class UserRegisterForm(UserCreationForm):
 
-	class Meta:
-		model = User
-		fields = ['username','email']
-		help_texts = {
-		'username': '',
-		'password1':'',
-		'password2':''
-		}
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        help_texts = {
+            'username': '',
+            'password1': '',
+            'password2': ''
+        }
+
+
 class UserLoginForm(forms.Form):
 
-	username = forms.CharField()
-	password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
 
 class RunCreationForm(ModelForm):
+    class Meta:
+        model = RunInfo
+        fields = ['Flowcell_ID', 'date', 'read_type', 'read_length']
+        widgets = {
+            'date': forms.DateInput(),
+            # 'read_type': forms.TextInput(attrs={'class': 'myfieldclass'}),
+        }
 
-	class Meta:
-		model = RunInfo
-		fields = ['Flowcell_ID','date','read_type','read_length']
-		widgets ={
-			 'date': forms.DateInput(),
 
-		}
 class LibrariesInRunForm(ModelForm):
+    class Meta:
+        model = LibrariesInRun
+        fields = ['Library_ID', 'i7index', 'i5index']
 
-	class Meta:
-		model = LibrariesInRun
-		fields = ['Library_ID','i7index','i5index']
 
 class SamplesToCreatForm(forms.Form):
-	samplestocreat = forms.CharField(
-			label='Libraries to Save:',
-			widget=forms.Textarea(attrs={'cols': 60, 'rows': 20}),
-			initial='Library_ID i7index i5index(separated by Space or Tab)\n'
-		)
+    samplestocreat = forms.CharField(
+        label='Libraries to Save:',
+        widget=forms.Textarea(attrs={'cols': 60, 'rows': 20}),
+        initial='Library_ID i7index i5index(separated by Space or Tab)\n'
+    )
--- nextseq_app/models.py
+++ nextseq_app/models.py
@@ -2,46 +2,63 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class Barcode(models.Model):
-	indexid = models.CharField(max_length=200,unique=True)
-	indexseq = models.CharField(max_length=200)
-	def __str__(self):
-		return self.indexid
-	class Meta:
-		ordering = ['indexid']
+    indexid = models.CharField(max_length=200, unique=True)
+    indexseq = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.indexid
+
+    class Meta:
+        ordering = ['indexid']
+
 
 class RunInfo(models.Model):
-	#runid = models.CharField(max_length=200,unique=True,help_text='Please enter the flowcellSe')
-	Flowcell_ID  = models.CharField(max_length=200,unique=True, help_text='Please enter the FlowcellSerialNumber, like H5GLYBGX5')
-	operator = models.ForeignKey(User, on_delete=models.CASCADE)
-	date = models.DateField('I did this run on...',help_text='If the datepicker is not working, please enter in this form: yyyy-mm-dd, like 2018-04-03',blank=True,null=True)
-	read_type_choice = (('SE','Single-end'),('PE','Paired-end'))
-	read_type = models.CharField(max_length=2,choices=read_type_choice)
-	total_reads = models.IntegerField(blank=True,null=True)
-	total_libraries = models.IntegerField(blank=True,null=True)
-	percent_of_reads_demultiplexed = models.IntegerField(blank=True,null=True)
-	read_length = models.CharField(max_length=50,help_text='e.g. if R1=R2=75, enter 75, if R1=50,R2=75, enter 50+75')
-	updated_at = models.DateTimeField(auto_now=True)
-	nextseqdir = models.CharField(max_length=200,blank=True,null=True)
+    # runid = models.CharField(max_length=200,unique=True,help_text='Please enter the flowcellSe')
+    Flowcell_ID = models.CharField(
+        max_length=200, unique=True, help_text='Please enter the FlowcellSerialNumber, like H5GLYBGX5')
+    operator = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField('I did this run on...',
+                            help_text='If the datepicker is not working, please enter in this form: yyyy-mm-dd, like 2018-04-03', blank=True, null=True)
+    read_type_choice = (('SE', 'Single-end'), ('PE', 'Paired-end'))
+    exp_type_choice = (('BK', 'bulk'), ('SN', 'snATAC'),
+                       ('TA', '10xATAC'), ('TR', '10xRNA'))
+    read_type = models.CharField(
+        max_length=2, choices=read_type_choice, default='PE')
+    #exp_type = models.CharField(max_length=2, choies=exp_type_choice)
+    total_reads = models.IntegerField(blank=True, null=True)
+    total_libraries = models.IntegerField(blank=True, null=True)
+    percent_of_reads_demultiplexed = models.IntegerField(blank=True, null=True)
+    read_length = models.CharField(
+        max_length=50, help_text='e.g. if R1=R2=75, enter 75, if R1=50,R2=75, enter 50+75')
+    updated_at = models.DateTimeField(auto_now=True)
+    nextseqdir = models.CharField(max_length=200, blank=True, null=True)
 
-	jobstatus = models.CharField(max_length=200,blank=True,null=True,default='ClickToSubmit')
+    jobstatus = models.CharField(
+        max_length=200, blank=True, null=True, default='ClickToSubmit')
 
-	def get_absolute_url(self):
-		return reverse('nextseq_app:rundetail', kwargs={'pk': self.pk})
+    def get_absolute_url(self):
+        return reverse('nextseq_app:rundetail', kwargs={'pk': self.pk})
 
-	def __str__(self):
-		return self.Flowcell_ID
+    def __str__(self):
+        return self.Flowcell_ID
 
-	class Meta:
-		ordering = ['-date']
+    class Meta:
+        ordering = ['-date']
+
 
 class LibrariesInRun(models.Model):
-	singlerun = models.ForeignKey(RunInfo, on_delete=models.CASCADE)
-	Library_ID = models.CharField(max_length=200,unique=True)
-	i7index = models.ForeignKey(Barcode,related_name='i7_index', on_delete=models.CASCADE, blank=True, null=True)
-	i5index = models.ForeignKey(Barcode,related_name='i5_index',on_delete=models.CASCADE, blank=True, null=True)
-	numberofreads = models.IntegerField(blank=True,null=True)
-	def __str__(self):
-		return self.Library_ID
-	class Meta:
-		ordering = ['Library_ID']
+    singlerun = models.ForeignKey(RunInfo, on_delete=models.CASCADE)
+    Library_ID = models.CharField(max_length=200, unique=True)
+    i7index = models.ForeignKey(Barcode, related_name='i7_index',
+                                on_delete=models.CASCADE, blank=True, null=True)
+    i5index = models.ForeignKey(Barcode, related_name='i5_index',
+                                on_delete=models.CASCADE, blank=True, null=True)
+    numberofreads = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.Library_ID
+
+    class Meta:
+        ordering = ['Library_ID']
