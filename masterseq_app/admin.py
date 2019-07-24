2f5040a3e951c7eae4e1ab2492df3b37ec05639b
biomystery <zhangc518@gmail.com>
2019-07-24 13:48:34 -0700

    added few models in admin view

--- masterseq_app/admin.py
+++ masterseq_app/admin.py
@@ -1,19 +1,35 @@
 from django.contrib import admin
-from .models import ProtocalInfo,SeqMachineInfo,GenomeInfo
+from .models import ProtocalInfo, SeqMachineInfo, GenomeInfo, SampleInfo, SeqInfo, SeqBioInfo
 
 # Register your models here.
-class GenomeInfoAdmin(admin.ModelAdmin):
-	list_display = ('species','genome_name')
 
-admin.site.register(GenomeInfo,GenomeInfoAdmin)
+
+class GenomeInfoAdmin(admin.ModelAdmin):
+    list_display = ('species', 'genome_name')
+
+
+admin.site.register(GenomeInfo, GenomeInfoAdmin)
+
 
 class SeqMachineInfoAdmin(admin.ModelAdmin):
-	list_display = ('sequencing_core','machine_name')
+    list_display = ('sequencing_core', 'machine_name')
 
-admin.site.register(SeqMachineInfo,SeqMachineInfoAdmin)
+
+admin.site.register(SeqMachineInfo, SeqMachineInfoAdmin)
+
 
 class ProtocalInfoAdmin(admin.ModelAdmin):
-	list_display = ('experiment_type','protocal_name')
+    list_display = ('experiment_type', 'protocal_name')
 
-admin.site.register(ProtocalInfo,ProtocalInfoAdmin)
 
+admin.site.register(ProtocalInfo, ProtocalInfoAdmin)
+
+
+@admin.register(SampleInfo)
+class SampleInfoAdmin(admin.ModelAdmin):
+    list_display = ('sample_id', 'sample_type', 'service_requested', 'group')
+
+
+@admin.register(SeqInfo)
+class SeqInfoAdmin(admin.ModelAdmin):
+    list_display = ('seq_id', 'total_reads', 'machine')
--- setqc_app/admin.py
+++ setqc_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import LibrariesSetQC, LibraryInSet
 
 # Register your models here.
+
+
+@admin.register(LibrariesSetQC)
+class SetQCAdmin(admin.ModelAdmin):
+    list_display = ('set_id', 'set_name', 'experiment_type')
