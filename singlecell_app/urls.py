0ad2efaf85e74421a1a8a2bfa1eb4d30f230d640
brg029@ucsd.edu <brg029@ucsd.edu>
2019-10-03 07:40:12 -0700

    adding sequences views url to singlecell app, include columns for 10xProcessed and button/link column for CoolADmin

--- singlecell_app/urls.py
+++ singlecell_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'singlecell_app'
 urlpatterns = [
     path('mysclibs', views.scIndex, name='scindex'),
-    path('allsclibs', views.AllScLibs, name='allsclibs')
+    path('allsclibs', views.AllScLibs, name='allsclibs'),
+    path('AllSeqs', views.AllSeqs, name='allseqs'),
+    path('MySeqs', views.MySeqs, name='myseqs'),
+    
 ]
--- singlecell_app/views.py
+++ singlecell_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from masterseq_app.models import LibraryInfo
+from masterseq_app.models import LibraryInfo, SeqInfo
+from django.conf import settings
+import os
+import random
+
+
 # Create your views here.
 
 '''
@@ -22,10 +27,12 @@ def scIndex(request):
         ('Experiment Type','experiment_type'),
         ('Date Started' ,'date_started'),
         ('Date Completed' ,'date_completed'),
+        ('CoolAdmin','CoolAdmin')
     ]
 
-    library_lists = BuildLibraryList(library_list, header)
+    library_lists = BuildList(library_list, header)
     print(library_lists)
+    
     context = {
         'type' : 'My Single Cell Libraries',
         'header' : header,
@@ -45,7 +52,7 @@ def AllScLibs(request):
         ('Date Started' ,'date_started'),
         ('Date Completed' ,'date_completed'),
     ]
-    library_lists = BuildLibraryList(library_list, header)
+    library_lists = BuildList(library_list, header)
     print(library_lists)
     context = {
         'type' : 'All Single Cell Libraries',
@@ -54,11 +61,163 @@ def AllScLibs(request):
     }
     return render(request, 'singlecell_app/scIndex.html', context)
 
-def BuildLibraryList(library_list, header):
-    library_lists = []
-    for library in library_list:
+'''Use to build library lists
+'''
+def BuildList(item_list, header):
+    lists = []
+    for item in item_list:
         properties = {}
         for head in header:
-                properties[head[1]] = library[head[1]]
-        library_lists.append(properties)
-    return library_lists
+            try:
+                property = item[head[1]]
+                properties[head[1]] = property
+            except:
+                print('property not found: ', head[1])
+                properties[head[1]] = ''
+        lists.append(properties)
+    return lists
+
+def find10xStatus(seq_ids):
+    tenxdir = settings.TENX_DIR
+    tenx_output_folder = 'outs'
+    tenx_target_outfile = 'web_summary.html'
+
+    tenxstatus = []
+    print('in tenx function')
+    for seq in seq_ids:
+        seq = str(seq)
+        print(f'seq:{seq}')
+        if not os.path.isdir(os.path.join(tenxdir, seq)):
+            tenxstatus.append(False)
+        else:
+            if not os.path.isfile( os.path.join( tenxdir, seq, tenx_output_folder, tenx_target_outfile) ):
+                tenxstatus.append(False)
+            else:
+                tenxstatus.append(True)
+    return tenxstatus
+
+def findSeqStatus(seq_ids):
+    fastqdir = settings.FASTQ_DIR
+    seqsStatus=[]
+    for seq in seq_ids:
+        seq_id = seq.seq_id
+        reps =  seq_id.split('_')[2:]
+        mainname = '_'.join(seq_id.split('_')[0:2])
+        #rep is brandon_210_2 or brandon_210_3 in brandon_210_1_2_3
+        print(reps)
+        if len(reps) == 0:
+            print('main: ',mainname)
+        else:
+            for rep in reps:
+                if rep == '1':
+                    print(rep)
+                    if seq.read_type == 'PE':
+                        r1 = mainname + 'R1_fastq.gz'
+                        r2 = mainname +'_R2.fastq.gz'
+                        if not os.path.isfile(os.path.join(fastqdir, r1)) or not os.path.isfile(os.path.join(fastqdir, r2)):
+                            seqsStatus.append('No')
+                        else:
+                            seqs10xStatus.append('Yes')
+                    else:
+                        r1 = mainname+'.fastq.gz'
+                        r1op = item+'_R1.fastq.gz'
+                        if not os.path.isfile(os.path.join(fastqdir, r1)) and not os.path.isfile(os.path.join(fastqdir, r1op)):
+                            seqsStatus.append('No')
+                        else: 
+                            seqsStatus.append('Yes')
+
+                else:
+                    #find mainname_rep
+                    print(rep)
+                    repname = mainname + '_' + rep
+                    if seq.read_type == 'PE':
+                        r1 = repname + 'R1_fastq.gz'
+                        r2 = repname +'_R2.fastq.gz'
+                        if not os.path.isfile(os.path.join(fastqdir, r1)) or not os.path.isfile(os.path.join(fastqdir, r2)):
+                            seqsStatus.append('No')
+                        else:
+                            seqs10xStatus.append('Yes')
+                    else:
+                        r1 = repname+'.fastq.gz'
+                        r1op = item+'_R1.fastq.gz'
+                        if not os.path.isfile(os.path.join(fastqdir, r1)) and not os.path.isfile(os.path.join(fastqdir, r1op)):
+                            seqsStatus.append('No')
+                        else:                    
+                            seqsStatus.append('Yes')
+    return seqsStatus
+
+'''Use to build seq lists
+'''
+def BuildSeqList(seqs_list, header):
+    seqs_info = []
+    seqsStatus = findSeqStatus(seqs_list)
+    seqs10xStatus = find10xStatus(seqs_list)
+    i = 0
+    for seq in seqs_list:
+        properties = {}
+        properties['libraryinfo_id'] = seq.libraryinfo_id
+        properties['seq_id'] = seq.seq_id
+        properties['experiment_type'] = seq.libraryinfo.experiment_type
+        properties['date_submitted_for_sequencing'] = seq.date_submitted_for_sequencing
+        properties['Sequence Status'] = seqsStatus[i]
+        properties['10xProcessed'] = seqs10xStatus[i]
+        properties['CoolAdmin'] = FindCoolAdminStatus(seq)
+        seqs_info.append(properties)
+        i+=1
+
+    return seqs_info
+
+def FindCoolAdminStatus(seq):
+    i = random.randint(1,10)
+    #do something
+    if i > 7:
+        return 'No'
+    elif i > 4:
+        return 'Yes'
+    else: 
+        return 'Submitted'
+
+def AllSeqs(request):
+    seqs_list = SeqInfo.objects.filter(libraryinfo__experiment_type__in=SINGLE_CELL_EXPS).select_related('libraryinfo')
+    
+    header = [
+        ('Sequence ID','seq_id'),
+        ('Library ID', 'libraryinfo_id'),
+        ('Experiment Type','experiment_type'),
+        ('Date Submitted for Sequencing' ,'date_submitted_for_sequencing'),
+        ('Sequence Status' ,'Sequence Status'),
+        ('10xProcessed','10xProcessed'),
+        ('CoolAdmin', 'CoolAdmin')
+    ]
+    print(seqs_list)
+    seqs_info = BuildSeqList(seqs_list, header)
+    print(seqs_info)
+    # this for loop we will get 10xProcessed status, sequence status, and Experiment type 
+    print(seqs_info)
+   
+    context = {
+        'type':'All Sequences',
+        'header': header,
+        'seqs_info': seqs_info,
+    }
+    return render(request,'singlecell_app/seqs.html',context)
+
+def MySeqs(request):
+    header = [
+        ('Sequence ID','seq_id'),
+        ('Library ID', 'libraryinfo_id'),
+        ('Experiment Type','experiment_type'),
+        ('Date Submitted for Sequencing' ,'date_submitted_for_sequencing'),
+        ('Sequence Status' ,'Sequence Status'),
+        ('10xProcessed','10xProcessed'),
+        ('CoolAdmin', 'CoolAdmin')
+    ]
+    seqs_list = SeqInfo.objects.filter(libraryinfo__experiment_type__in=SINGLE_CELL_EXPS, team_member_initails=request.user).select_related('libraryinfo')
+    seqs_info = BuildSeqList(seqs_list, header)
+    context = {
+        'type':'My Sequences',
+        'header': header,
+        'seqs_info': seqs_info,
+    }
+    return render(request,'singlecell_app/seqs.html',context)
+
