c83d06df0c5ba133041756d7e897d22d9446ccdc
biomystery <zhangc518@gmail.com>
2019-12-13 13:45:41 -0800

    isolate cooladmin scripts

--- utility/coolAdmin.sh
+++ utility/coolAdmin.sh
@@ -5,6 +5,9 @@ SEQ=$2
 PIPELINE=$3
 PARAMSTRING=$4
 cmd1="conda activate /home/opoirion/prog/conda_env/"
+echo $cmd1
 cmd2="qsub -v OUTPUTNAME=${SEQ},INPUTNAME=${SEQ},DATASETNAME=${SEQ},${PARAMSTRING} /home/opoirion/data/ps-epigen_job/LIMS/10x_model.bash"
-job1=$(ssh brg029@tscc-login.sdsc.edu ${cmd1}; ${cmd2})
+echo $cmd2
+
+job1=$(ssh zhc268@tscc-login.sdsc.edu ${cmd1}; ${cmd2})
 
