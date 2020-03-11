5f679d1317b1c9e931b2e775d1be627f669b503c
liyuxin <liyuxin@liyuxins-iMac.local>
2020-03-11 10:57:12 -0700

    initiate fee for service model

--- collaborator_app/models.py
+++ collaborator_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
+
+class ServiceInfo(models.Model):
+    service_name = models.CharField(max_length=100,unique=True)
+    uc_rate = models.FloatField(blank=True, null=True)
+    nonuc_rate = models.FloatField(blank=True, null=True)
+    rate_unit = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.service_name
+
+class Request(models.Model):
+    quote_number= models.CharField('quote number', max_length=100,unique=True,blank=True, null=True)
+    #service_request = models.ManyToManyField(ServiceRequest)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
+    date =  models.DateField(blank=True, null=True)
+    notes = models.TextField(blank=True)
+
+class ServiceRequest(models.Model):
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    service = models.ForeignKey(ServiceInfo, on_delete=models.CASCADE)
+    quantity = models.FloatField(blank=True, null=True)
+    status = models.CharField(max_length=20, blank=True, null=True)
--- db/README.md
+++ db/README.md
@@ -27,6 +27,7 @@ ALTER USER test WITH PASSWORD '123456';
 * [nextseq@dbdiagram.io](https://dbdiagram.io/d/5c3d227619c125001489f837)
 * [metadata@dbdiagram.io](https://dbdiagram.io/d/5bb396d8e63c1f0014dab57d)
 * [usrs@dbdigram.io](https://dbdiagram.io/d/5c05a310b155a200149def72)
+* [FeeForServiceModel](https://dbdiagram.io/d/5e691c144495b02c3b881e6d)
 
 * [CoolAdminModel](https://dbdiagram.io/d/5dc0512fedf08a25543d7eb5)
 
