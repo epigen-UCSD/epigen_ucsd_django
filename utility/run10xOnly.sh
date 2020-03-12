c700d3d4ddbf88d3cd3c5a1cbce4beeed8f3e990
biomystery <zhangc518@gmail.com>
2020-03-12 12:51:03 -0700

    fix 10xATAC submission in snAPP

--- utility/run10xOnly.sh
+++ utility/run10xOnly.sh
@@ -8,7 +8,14 @@ TENXFILE=${TENX_DIR}${SEQ}"/."${SEQ}".tsv"
 TYPE2='10xATAC'
 #only one job shuold be submitted with this
 n_libs=$( wc -l $TENXFILE |  awk '{print $1}'  )
-cmd1="qsub -t 0-$[n_libs-1] -v samples=${TENXFILE} -M $USER_EMAIL -q hotel -l walltime=24:00:00 \$(which run10xPipeline.pbs)"
+
+if [ $n_libs -gt 1 ]
+then
+    cmd1="qsub -t 0-$[n_libs-1] -v samples=${TENXFILE} -M $USER_EMAIL -q hotel -l walltime=24:00:00 \$(which run10xPipeline.pbs)"
+else
+    cmd1="qsub -v samples=${TENXFILE} -M $USER_EMAIL -q hotel -l walltime=24:00:00 \$(which run10xPipeline.pbs)"
+fi
+
 echo "${cmd1}"
 job1=$(ssh zhc268@tscc-login.sdsc.edu $cmd1)
     
