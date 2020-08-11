0248f6524539c0332b274ae47d5f27b0b93a9002
biomystery <zhangc518@gmail.com>
2020-08-11 16:25:45 -0700

    fix bugs in demux 10x_atac_rna

--- utility/updateSingleCellStatus.py
+++ utility/updateSingleCellStatus.py
@@ -38,12 +38,12 @@ def main(seq_id, status):
     # for example: python updateSingelCellStatus.py -sedid 'MM_130' -status 'InProcess'
     sc_obj = SingleCellObject.objects.get(seqinfo__seq_id=seq_id)
     sc_obj.tenx_pipeline_status = status
-    if(status == 'Yes' or status == 'Error!'):
-        sc_obj.date_last_modified = datetime.now()
+    sc_obj.date_last_modified = datetime.now()
+
     if(status == 'Yes'):
         # check if there is an error file that exists
         dir_to_expts = settings.TENX_DIR if sc_obj.experiment_type == "10xATAC" else settings.SCRNA_DIR
-        error_file = sc_obj.experiment_type
+        # TODO: error handling error_file = sc_obj.experiment_type
         # if()
         # make qc metrics table and save it to SCmodel's generic foreign key
         sc_obj.content_object = generate_qc_metrics_table(
