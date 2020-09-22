cc62e869d018123424ab8b1e407bb92a8a47cc41
biomystery <zhangc518@gmail.com>
2020-09-22 00:08:32 -0700

    simplify demux 10xRNA, extraP already wrote into file

--- utility/runDemux10xRNA.sh
+++ utility/runDemux10xRNA.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 ## cmd1: qsub job from tscc 
-flowcell=$1; rundir=$2; useremail=$3; extraPars=${@:4:5};
-if [ -z $extraPars ]
-then
-    cmd1="qsub -k oe  -M $useremail -v flowcell_id=$flowcell,run_dir=$rundir \$(which runDemux10xRNA.pbs)"
-else
-    cmd1="qsub -k oe  -M $useremail -v flowcell_id=$flowcell,run_dir=$rundir,extraPars=\"$extraPars\" \$(which runDemux10xRNA.pbs)"
-fi
+flowcell=$1; rundir=$2; useremail=$3; 
+cmd1="qsub -k oe  -M $useremail -v flowcell_id=$flowcell,run_dir=$rundir \$(which runDemux10xRNA.pbs)"
 
 echo $cmd1
 ssh zhc268@tscc-login.sdsc.edu $cmd1
