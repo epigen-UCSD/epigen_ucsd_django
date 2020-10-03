5827464643d021823d27b8a2d51b4aee1decb18c
biomystery <zhangc518@gmail.com>
2020-10-03 10:55:56 -0700

    fix cooladmin edit button for 10x final

--- static/js/epigen.js
+++ static/js/epigen.js
@@ -2112,7 +2112,7 @@ $(document).ready(function () {
                 "targets": 8,
                 "render": function (data, type, row) {
                     var seq_id = row['seqinfo__seq_id']
-                    if (row['seqinfo__libraryinfo__experiment_type'] !== "10xATAC" || row['10x_status'] === "Results") {
+                    if (row['seqinfo__libraryinfo__experiment_type'] !== "10xATAC" || row['tenx_pipeline_status'] === "Yes") {
                         return ('<a href="/singlecell/EditCoolAdmin/' + seq_id + '"><i class="fas fa-edit"></i></a>');
                     } else {
                         return ('<a href="#"><i class="fas fa-edit"></i></a>')
