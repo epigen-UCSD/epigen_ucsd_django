ae739ac710523f1db95bc7998de3266166aad006
biomystery <zhangc518@gmail.com>
2020-11-04 17:05:29 -0800

    update bash for TA including dual index RNA

--- utility/runDemux10xATAC_RNA.sh
+++ utility/runDemux10xATAC_RNA.sh
@@ -7,7 +7,17 @@ flowcell=$1; rundir=$2; useremail=$3; extraPars=${@:4:5};
 cd $out_dir
 samplesheet=$(find ${rundir}/ -name "SampleSheet.csv" )
 barcodefile=/home/zhc268/epigen_ucsd_django/data/nextseq_app/barcodes/chromium-shared-sample-indexes-plate.csv
-convertTASampleSheet.py -b $barcodefile -s ${samplesheet}|grep -v "SI-NA"> ${samplesheet/.csv/_rna_expand.csv}
+barcodefile_TT=/home/zhc268/epigen_ucsd_django/data/nextseq_app/barcodes/Dual_Index_Kit_TT_Set_A.csv
+
+if [[ $(grep "SI-GA" ${samplesheet}) ]]
+then
+    convertTASampleSheet.py -b $barcodefile -s ${samplesheet}|grep -v "SI-NA" |\
+        grep -v "SI-TT" > ${samplesheet/.csv/_rna_expand.csv}
+elif [[ $(grep "SI-TT" ${samplesheet}) ]]
+then 
+    convertTASampleSheet_TT.py -b $barcodefile_TT -s ${samplesheet}|grep -v "SI-NA" |\
+        grep -v "SI-GA" > ${samplesheet/.csv/_rna_expand_TT.csv}
+fi
 
 ## cmd1: qsub job from tscc 
 if [ -z $extraPars ]
