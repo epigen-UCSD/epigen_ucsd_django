a889d1b368c773848fc1cc1ee722489da4561ea5
biomystery <zhangc518@gmail.com>
2020-12-10 10:49:45 -0800

    increase default walltime for 10xrna

--- utility/runCellRanger.sh
+++ utility/runCellRanger.sh
@@ -11,6 +11,6 @@ TENXFILE=${TENX_DIR}${SEQ}"/."${SEQ}".tsv"
 TYPE2='10xCellRangerRNA' 
 #only one job shuold be submitted with this
 n_libs=$( wc -l $TENXFILE |  awk '{print $1}'  )
-cmd1="qsub -k oe -t 0-$[n_libs-1] -v samples=${TENXFILE},genome_path=${REF_GEN_LOCATION} -M $USER_EMAIL -q hotel -l walltime=24:00:00 \$(which run10xCellRanger.pbs)"
+cmd1="qsub -k oe -t 0-$[n_libs-1] -v samples=${TENXFILE},genome_path=${REF_GEN_LOCATION} -M $USER_EMAIL -q hotel -l walltime=48:00:00 \$(which run10xCellRanger.pbs)"
 echo "${cmd1}"
 job1=$(ssh zhc268@tscc-login.sdsc.edu $cmd1)
