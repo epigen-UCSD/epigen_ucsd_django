6ff7380062c33c97d3b05bcb85e3f59e8ede8307
liyuxin <liyuxin@liyuxins-iMac.local>
2021-02-07 15:31:19 -0800

    fixed a bug when installing the LIMS in another machine, avoiding modifying form.py, but now fixed partially, only for setQC app

--- setqc_app/forms.py
+++ setqc_app/forms.py
@@ -152,8 +152,7 @@ class SeqLabelGenomeCreationForm(forms.Form):
     sequencingid = forms.CharField(label='Sequencing ID', disabled=True)
     speciesbelong = forms.CharField(
         label='Species', disabled=True, widget=forms.TextInput(attrs={'size': '8'}))
-    genomeinthisset = forms.ChoiceField(label='Genome', choices=[(
-        x.genome_name, x.genome_name) for x in GenomeInfo.objects.all()])  # [('hg38','hg38')])
+    genomeinthisset = forms.ChoiceField(choices = [])  # [('hg38','hg38')])
 
     lableinthisset = forms.CharField(label='Label')
 
